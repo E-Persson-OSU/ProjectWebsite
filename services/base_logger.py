a08ef7210d3e579182cad34a0e64b5aa0ce60150
E-Persson-OSU <osuepersson@gmail.com>
2023-04-28 08:16:42 -0400

    fixed logger

--- services/base_logger.py
+++ services/base_logger.py
@@ -4,11 +4,13 @@ from pathlib import Path
 LOGGING_PATH = Path("services/logs/") / "weblogs.log"
 FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
-logging.basicConfig(
-    filename=LOGGING_PATH,
-    filemode="w",
-    format=FORMAT,
-    level=logging.ERROR,
-)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 
-logger = logging
+# create a file handler
+file_handler = logging.FileHandler(LOGGING_PATH)
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(logging.Formatter(FORMAT))
+
+# add the handlers to the logger
+logger.addHandler(file_handler)
