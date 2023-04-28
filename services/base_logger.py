1670bab22ae5857add47d6f9c734a10ca72a41c4
E-Persson-OSU <osuepersson@gmail.com>
2023-04-28 08:21:25 -0400

    logging fixed?

--- services/base_logger.py
+++ services/base_logger.py
@@ -1,8 +1,12 @@
 import logging
 from pathlib import Path
+import os
 
 LOGGING_PATH = Path("services/logs/") / "weblogs.log"
 FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+if not LOGGING_PATH.parent.exists():
+    os.makedirs(LOGGING_PATH.parent)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
